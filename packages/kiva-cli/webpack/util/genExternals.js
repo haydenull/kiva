0df037f9a82145bd5c9c6a755d0a600f79101d87
Haaayden <brooklyn1984@qq.com>
2020-10-12 08:21:56 +0800

    fix(kiva-cli): :bug: 修复不存在utils mxins 时文件夹读取错误

--- packages/kiva-cli/webpack/util/genExternals.js
+++ packages/kiva-cli/webpack/util/genExternals.js
@@ -10,21 +10,30 @@ const baseDir = process.cwd()
 let externals = {}
 
 const componentsList = fs.readdirSync(path.resolve(baseDir, './src/components'))
-const utilsList = fs.readdirSync(path.resolve(baseDir, './src/utils'))
-const mixinsList = fs.readdirSync(path.resolve(baseDir, './src/mixins'))
-
 componentsList.forEach(file => {
   file = path.basename(file, '.js')
   externals[`@ui/src/components/${file}`] = `${packageName}/lib/${file}`
 })
-utilsList.forEach(file => {
-  file = path.basename(file, '.js')
-  externals[`@ui/src/utils/${file}`] = `${packageName}/lib/utils/${file}`
-})
-mixinsList.forEach(file => {
-  file = path.basename(file, '.js')
-  externals[`@ui/src/mixins/${file}`] = `${packageName}/lib/mixins/${file}`
-})
+
+try {
+  const utilsList = fs.readdirSync(path.resolve(baseDir, './src/utils'))
+  utilsList.forEach(file => {
+    file = path.basename(file, '.js')
+    externals[`@ui/src/utils/${file}`] = `${packageName}/lib/utils/${file}`
+  })
+} catch (error) {
+  console.log('未发现需要提出的 utils')
+}
+
+try {
+  const mixinsList = fs.readdirSync(path.resolve(baseDir, './src/mixins'))
+  mixinsList.forEach(file => {
+    file = path.basename(file, '.js')
+    externals[`@ui/src/mixins/${file}`] = `${packageName}/lib/mixins/${file}`
+  })
+} catch (error) {
+  console.log('未发现需要提出的 mixins')
+}
 
 externals = [Object.assign({
   vue: 'vue',
